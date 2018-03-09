88a2dc9b4587968237e128168b872f2e20418496
Matthieu <matthieupasserel@gmail.com>
2018-03-09 17:19:45 +0100

    Refactoriser le code

---
+++ ColorBee/Model/Joueur.swift
@@ -0,0 +1,27 @@
+//
+//  Joueur.swift
+//  ColorBee
+//
+//  Created by Matthieu PASSEREL on 09/03/2018.
+//  Copyright © 2018 Matthieu PASSEREL. All rights reserved.
+//
+
+import SpriteKit
+
+class Joueur: SKShapeNode {
+
+    func miseEnPlace(scene: SKScene) {
+        fillColor = .blue
+        strokeColor = .blue
+        position = CGPoint(x: scene.size.width / 2, y: scene.size.height / 2)
+        let body = SKPhysicsBody(circleOfRadius: 25)
+        body.affectedByGravity = true
+        body.mass = 1.5
+        physicsBody = body
+    }
+    
+    func sauter() {
+        physicsBody?.velocity.dy = 500
+    }
+    
+}
